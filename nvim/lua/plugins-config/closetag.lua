6b997769e039755af1a8de59f69552932af63de5
Mats Dahlin <pm@matsdahlin.com>
2022-03-30 22:24:34 +0200

    remove extra whitespace

--- nvim/lua/plugins-config/closetag.lua
+++ nvim/lua/plugins-config/closetag.lua
@@ -1,6 +1,6 @@
 local let = vim.g
 
-let.closetag_filenames = '*.html,*.xhtml,*.phtml,*.js,*.jsx,*.ts,*.tsx, *.cshtml'
+let.closetag_filenames = '*.html,*.xhtml,*.phtml,*.js,*.jsx,*.ts,*.tsx,*.cshtml'
 let.closetag_emptyTags_caseSensitive = 1
 
 return {
